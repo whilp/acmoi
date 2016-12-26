64a6a37293882895c4d751feb204bc493dfaba89
Will Maier <wcmaier@gmail.com>
2016-12-25 21:38:51 -0800

    buncha hacks

--- cmd/Do/main.go
+++ cmd/Do/main.go
@@ -1,39 +1,45 @@
 package main
 
 import (
-	"bytes"
 	"flag"
-	"io/ioutil"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strconv"
+	"strings"
+	"sync"
 
 	"9fans.net/go/acme"
+	"9fans.net/go/plan9"
+	"9fans.net/go/plan9/client"
 )
 
 var (
-	fOnce = flag.Bool("once", false, "run once")
+	fDaemon = flag.Bool("daemon", false, "run in the background")
 )
 
 func main() {
-	err := inner()
+	err := run()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func inner() error {
+func run() error {
 	flag.Parse()
 
-	name := os.Getenv("%")
-	winid, err := strconv.Atoi(os.Getenv("winid"))
+	cons, err := NewCons()
 	if err != nil {
 		return err
 	}
-	if *fOnce {
-		return handle(name, winid)
+	defer cons.Close()
+	log.SetOutput(cons)
+
+	if !*fDaemon {
+		return once()
 	}
 
 	events, err := acme.Log()
@@ -41,97 +47,155 @@ func inner() error {
 		return err
 	}
 	defer events.Close()
+
 	for {
 		event, err := events.Read()
 		if err != nil {
 			return err
 		}
 		if event.Op == "put" && event.Name != "" {
-			handle(event.Name, event.ID)
+			err := handle(event.Name, event.ID)
+			if err != nil {
+				log.Print(err)
+			}
 		}
 	}
 }
 
+func once() error {
+	name := os.Getenv("%")
+	winid, err := strconv.Atoi(os.Getenv("winid"))
+	if err != nil {
+		return err
+	}
+
+	win, err := acme.Open(winid, nil)
+	if err != nil {
+		return err
+	}
+	defer win.CloseFiles()
+
+	if _, err := win.Write("ctl", []byte("put\n")); err != nil {
+		return err
+	}
+
+	return handle(name, winid)
+}
+
 func handle(name string, id int) error {
+	var err error
+
+	name, err = filepath.Abs(name)
+	if err != nil {
+		return err
+	}
+
 	win, err := acme.Open(id, nil)
 	if err != nil {
 		return err
 	}
 	defer win.CloseFiles()
 
-	if err := format(name, win); err != nil {
+	dir := toplevel(name) + "/"
+	_, err = WindowByName(dir)
+	if err != nil {
 		return err
 	}
 
-	if err := check(name, win); err != nil {
+	did, err := WindowID(dir)
+	if err != nil {
+		return err
+	}
+	e, err := NewErrors(did)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	handler := NewHandler(e, dir, win)
+	return handler.Handle(name, id)
+}
+
+type Handler struct {
+	win *acme.Win
+	out io.Writer
+	dir string
+}
+
+func NewHandler(out io.Writer, dir string, win *acme.Win) *Handler {
+	return &Handler{
+		win: win,
+		out: out,
+		dir: dir,
+	}
+}
+
+func (h *Handler) Handle(name string, id int) error {
+	if err := h.format(name); err != nil {
+		return err
+	}
+
+	if err := h.build(name); err != nil {
+		return err
+	}
+
+	if err := h.test(name); err != nil {
+		log.Printf("an err %s", err)
 		return err
 	}
 	return nil
 }
 
-func check(name string, win *acme.Win) error {
-	return nil
+func (h *Handler) run(cmd string, args ...string) *exec.Cmd {
+	c := exec.Command(cmd, args...)
+	c.Dir = h.dir
+	c.Stderr = h.out
+	c.Stdout = h.out
+	return c
 }
 
-func format(name string, win *acme.Win) error {
-	body, err := ioutil.ReadFile(name)
-	if err != nil {
-		return err
-	}
+func (h *Handler) check(name string) error {
+	return h.run("acme-check", name).Run()
+}
 
-	// TODO do this from the project root (ie, git toplevel)
-	cmd := exec.Command("acme-format", name)
-	cmd.Stderr = os.Stderr
+func (h *Handler) build(name string) error {
+	return h.run("acme-build", name).Run()
+}
 
-	result, err := cmd.Output()
-	if err != nil {
+func (h *Handler) test(name string) error {
+	return h.run("acme-test", name).Run()
+}
+
+func (h *Handler) format(name string) error {
+	cmd := h.run("acme-format", name)
+
+	if err := cmd.Run(); err != nil {
 		return err
 	}
 
 	// First addr read is always 0.
-	if _, _, err := win.ReadAddr(); err != nil {
+	if _, _, err := h.win.ReadAddr(); err != nil {
 		return err
 	}
 
-	if _, err := win.Write("ctl", []byte("addr=dot\n")); err != nil {
+	if _, err := h.win.Write("ctl", []byte("addr=dot\n")); err != nil {
 		return err
 	}
 
-	q0, q1, err := win.ReadAddr()
+	q0, q1, err := h.win.ReadAddr()
 	if err != nil {
 		return err
 	}
 
-	if bytes.Equal(body, result) {
-		return nil
-	}
-	if err := rewrite(win, result); err != nil {
-		return err
-	}
-	if err := show(win, q0, q1); err != nil {
-		return err
-	}
-	if err := put(win); err != nil {
+	if _, err := h.win.Write("ctl", []byte("get\n")); err != nil {
 		return err
 	}
 
-	return nil
-}
-
-func rewrite(win *acme.Win, body []byte) error {
-	// if _, err := win.Write("ctl", []byte("nomark\n")); err != nil {
-	//	return err
-	//}
-	// defer func() { win.Write("ctl", []byte("mark\n")) }()
-
-	if err := win.Addr("0,$"); err != nil {
+	if err := show(h.win, q0, q1); err != nil {
 		return err
 	}
 
-	_, err := win.Write("data", body)
-	if err != nil {
+	if _, err := h.win.Write("ctl", []byte("clean\n")); err != nil {
 		return err
-
 	}
 
 	return nil
@@ -148,35 +212,120 @@ func show(win *acme.Win, q0, q1 int) error {
 	return nil
 }
 
-func put(win *acme.Win) error {
-	_, err := win.Write("ctl", []byte("put\n"))
-	return err
-}
-
-type project struct {
-	root string
-}
-
-func NewProject(path string) *project {
-	r, err := root(path)
-	if err != nil {
-		r = os.Getenv("HOME")
-	}
-	return &project{
-		root: r,
-	}
-}
-
-func root(path string) (string, error) {
-	dir, _ := filepath.Split(path)
+func toplevel(path string) string {
+	dir := filepath.Dir(path)
 	cmd := exec.Command("git", "rev-parse", "--show-toplevel")
-	cmd.Stderr = os.Stderr
 	cmd.Dir = dir
 	out, err := cmd.Output()
 	if err != nil {
-		out = out[:len(out)-3]
+		return dir
 	}
-	r := string(out)
-	log.Printf("out %v", r)
-	return string(out), err
+	return strings.Trim(string(out), "\n")
+}
+
+// TODO contribute this to acme
+var fsys *client.Fsys
+var fsysErr error
+var fsysOnce sync.Once
+
+func mountAcme() {
+	fsys, fsysErr = client.MountService("acme")
+}
+
+type Cons struct {
+	f *client.Fid
+}
+
+func NewCons() (*Cons, error) {
+	fsysOnce.Do(mountAcme)
+	if fsysErr != nil {
+		return nil, fsysErr
+	}
+	f, err := fsys.Open("cons", plan9.ORDWR)
+	if err != nil {
+		return nil, err
+	}
+	return &Cons{f: f}, nil
+}
+
+func (c *Cons) Close() error {
+	return c.f.Close()
+}
+
+func (c *Cons) Write(b []byte) (int, error) {
+	return c.f.Write(b)
+}
+
+type Errors struct {
+	f *client.Fid
+}
+
+func NewErrors(id int) (*Errors, error) {
+	fsysOnce.Do(mountAcme)
+	if fsysErr != nil {
+		return nil, fsysErr
+	}
+
+	name := fmt.Sprintf("%d/errors", id)
+	f, err := fsys.Open(name, plan9.OWRITE)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Errors{f: f}, nil
+}
+
+func (e *Errors) Close() error {
+	return e.f.Close()
+}
+
+func (e *Errors) Write(b []byte) (int, error) {
+	return e.f.Write(b)
+}
+
+func WindowByName(name string) (*acme.Win, error) {
+	windows, err := acme.Windows()
+	if err != nil {
+		return nil, err
+	}
+	for _, w := range windows {
+		if w.Name == name {
+			return acme.Open(w.ID, nil)
+		}
+	}
+
+	win, err := acme.New()
+	if err != nil {
+		return nil, err
+	}
+	if err := win.Name(name); err != nil {
+		return nil, err
+	}
+	return win, nil
+}
+
+func WindowID(name string) (int, error) {
+	windows, err := acme.Windows()
+	if err != nil {
+		return 0, err
+	}
+	for _, w := range windows {
+		if w.Name == name {
+			return w.ID, nil
+		}
+	}
+	return 0, fmt.Errorf("could not find id for %s", name)
+}
+
+func WindowName(id int) (string, error) {
+	windows, err := acme.Windows()
+	if err != nil {
+		return "", err
+	}
+	for _, w := range windows {
+		if w.ID == id {
+			return w.Name, nil
+		}
+	}
+	return "", fmt.Errorf("could not find name for %d", id)
 }
