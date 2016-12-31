0f6850782171bd1bc3745036893aebb35349cc62
Will Maier <wcmaier@gmail.com>
2016-12-31 14:56:53 -0800

    Open: fix polling
    For now, create the window and poll until the window's ID disappears. A more elegant method that relies on the event log seems possible (and is implemented as waitevent()), but doesn't actually work. Weirdly, the event log seems to omit del events for the windows we create (but not others, and not non-del events).

--- cmd/Open/main.go
+++ cmd/Open/main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"path/filepath"
+	"time"
 
 	"9fans.net/go/acme"
 	"github.com/whilp/acmoi"
@@ -29,56 +31,62 @@ func run() error {
 		name = abs
 	}
 
-	cons, err := acmoi.NewCons()
+	if err := touch(name); err != nil {
+		return err
+	}
+
+	win, err := acmoi.NewWindowFromName(name)
 	if err != nil {
 		return err
 	}
-	defer cons.Close()
-	log.SetOutput(cons)
-
-	win, err := acme.New()
-	if err != nil {
+	if err := win.Win.Ctl("get\n"); err != nil {
 		return err
 	}
 
-	if err := win.Name(name); err != nil {
-		return err
-	}
-
-	w, err := acmoi.NewWindow(win)
-	if err != nil {
-		return err
-	}
-
-	parent, err := w.Parent()
-	if err != nil {
-		return err
-	}
-	w.Errors = parent.Errors
-
-	return edit(w)
+	return wait(win.Ctl.ID())
 }
 
-func edit(win *acmoi.Window) error {
+func touch(name string) error {
+	f, err := os.OpenFile(name, os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.Write([]byte{})
+	return err
+}
+
+func wait(id int) error {
+	for {
+		_, err := acmoi.NewWindowFromID(id)
+		if err != nil {
+			return nil
+		}
+		time.Sleep(200 * time.Millisecond)
+	}
+	return fmt.Errorf("missed del for winid %d", id)
+}
+
+func waitevent(name string) error {
 	events, err := acme.Log()
 	if err != nil {
 		return err
 	}
 	defer events.Close()
-	if err := win.Win.Ctl("get\n"); err != nil {
-		return err
-	}
 
 	for {
 		event, err := events.Read()
 		if err != nil {
 			return err
 		}
+		if event.Op == "focus" {
+			continue
+		}
 		log.Printf("event %v", event)
 
-		if event.Op == "del" && event.Name == win.Ctl.Name() {
+		if event.Op == "del" && event.Name == name {
 			return nil
 		}
 	}
-	return fmt.Errorf("missed del for %s", win.Ctl.Name())
+	return fmt.Errorf("missed del for %s", name)
 }
