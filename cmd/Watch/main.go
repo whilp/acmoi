9360f50b6886eaebc560fccf36e442c037d94f30
Will Maier <wcmaier@gmail.com>
2016-12-31 17:43:01 -0800

    Watch: tweak sigh

--- cmd/Watch/main.go
+++ cmd/Watch/main.go
@@ -14,8 +14,7 @@ When it sees such an event, it attempts to invoke each of the following external
 First, Watch determines the root of the project containing the file that has just been written by invoking `acme-root` with no arguments. Then, it invokes each command in sequence until it encounters an error, passing the path to the newly written file relative to the project root. The stdout and stderr of the commands are routed to the project root's +Errors window. `acme-format` is expected to rewrite the file on disk; if it succeeds, the window containing the file will be refreshed. Any command may return a non-zero status to stop further execution.
 
 */
-
-package main
+package main // import "github.com/whilp/acmoi/cmd/Watch"
 
 // TODO run stuff async
 // TODO Get strategy breaks undo across puts
